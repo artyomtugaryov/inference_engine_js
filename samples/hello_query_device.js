874693e2b856fae735847ac8a6be9562231a3c82
Yaroslav Tarkan <yatarkan@yandex.ru>
2019-10-31 19:37:08 +0300

    Add getting supported metrics from mock obj in query device sample

--- samples/hello_query_device.js
+++ samples/hello_query_device.js
@@ -1,19 +1,29 @@
+const { isString, isFinite, isArray } = require('lodash');
 const { IECore } = require('bindings')('InferenceEngineAddon');
 
 const ieCore = new IECore();
 
 const availableDevices = ieCore.getAvailableDevices();
 
+const mockMetricKey = 'MOCK_METRIC_KEY';
+const mockMetric = {
+    SUPPORTED_METRICS: [mockMetricKey],
+    [mockMetricKey]: 1,
+};
+
 // Mock function until bindings implemented
-ieCore.getMetric = (device, metricKey) => [{ [metricKey]:'Metric value placeholder' }];
+ieCore.getMetric = (device, metricKey) => mockMetric[metricKey];
 
 console.log('Available devices:');
 for (device of availableDevices) {
     console.log("\tDevice: ", device);
     console.log("\tMetrics:");
-    for (metric of ieCore.getMetric(device, 'MOCK_METRIC_KEY')) {
-        for (metricKey in metric) {
-            console.log(`\t\t${metricKey}: ${metric[metricKey]}`);
+    for (supportedMetricName of ieCore.getMetric(device, 'SUPPORTED_METRICS')) {
+        try {
+            const metric = ieCore.getMetric(device, supportedMetricName);
+            console.log(`\t\t${supportedMetricName}: ${metric}`);
+        } catch (e) {
+            console.log(`\t\t${supportedMetricName}: UNSUPPORTED TYPE`);
         }
     }
 }
