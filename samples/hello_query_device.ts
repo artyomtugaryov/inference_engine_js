9b1373defee47e87a473142a58f208bb2c96202f
ytarkanx <yaroslavx.tarkan@intel.com>
2019-12-05 15:44:41 +0300

    Add ts sample for query device

---
+++ samples/hello_query_device.ts
@@ -0,0 +1,20 @@
+const { Core } = require('bindings')('InferenceEngineJS');
+
+const ieCore = new Core();
+
+const availableDevices = ieCore.getAvailableDevices();
+
+
+console.log('Available devices:');
+for (let device of availableDevices) {
+    console.log("\tDevice: ", device);
+    console.log("\tMetrics:");
+    for (let supportedMetricName of ieCore.getMetric(device, 'SUPPORTED_METRICS')) {
+        try {
+            const metric = ieCore.getMetric(device, supportedMetricName);
+            console.log(`\t\t${supportedMetricName}: ${metric}`);
+        } catch (e) {
+            console.log(`\t\t${supportedMetricName}: UNSUPPORTED TYPE`);
+        }
+    }
+}
